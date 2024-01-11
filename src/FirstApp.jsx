029bf44818273059ff769c2ed283fbe332741929
tardio717 <tardiojaime@users.noreply.github.com>
2024-01-10 20:57:51 -0400

    impresion de variables

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -5,10 +5,21 @@ podemos utilizar Fragment de react, una forma mas rapida
 es utilizar <></> 
 puesto que un componente solo puede retornar un componente
 */
+/* 
+se recomienda utilizar variable fuera del componente variables 
+que no necesitan ser procesados
+react no puede imprimir objetos
+012 a continuacion
+*/
+const newMessage = {
+    message: 'First App',
+    title: 'Jaime Tardio'
+};
+
 export const FirstApp = () => {
   return (
     <>
-    <h1>First App</h1>
+    <h1> { JSON.stringify(newMessage) } </h1>
     <p>Tardio Jaime</p>
     </>
   )
